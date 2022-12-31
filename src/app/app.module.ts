4cf04c6efe2a9b25b6db3453ac48d9caa7302a01
vijay <vijay951mane@gmail.com>
2022-12-31 11:17:11 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultComponent } from './layouts/default/default.component';
+import { FullwidthComponent } from './layouts/fullwidth/fullwidth.component';
+import { HomeComponent } from './modules/home/home.component';
+import { LoginComponent } from './modules/login/login.component';
+import { PostsComponent } from './modules/posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultComponent,
+    FullwidthComponent,
+    HomeComponent,
+    LoginComponent,
+    PostsComponent,
+    
   ],
   imports: [
     BrowserModule,
